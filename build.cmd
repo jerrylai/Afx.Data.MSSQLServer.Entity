f62d64681cfdafea7049273922217be534feda23
jerrylai <jerrylai@aliyun.com>
2020-04-23 14:07:06 +0800

    add

---
+++ build.cmd
@@ -0,0 +1,12 @@
+@echo off
+set Build="%SYSTEMDRIVE%\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
+if exist publish rd /s /q publish
+%Build% "NET40/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
+%Build% "NET45/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
+dotnet build "NETStandard2.0/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release 
+cd publish
+del /q/s *.pdb
+del /q/s EntityFramework*
+del /q/s Afx.Data.Entity.dll Afx.Data.Entity.dll.config Afx.Data.Entity.xml
+del /q/s Afx.Data.dll Afx.Data.xml Afx.Data.MSSQLServer.Entity.dll.config
+pause
---
+++ src/Afx.Data.MSSQLServer.Entity/MsSqlDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+#if NETCOREAPP || NETSTANDARD
+using Microsoft.Data.SqlClient;
+#else
+using System.Data.SqlClient;
+#endif
+using Afx.Data;
+
+namespace Afx.Data.MSSQLServer.Entity.Schema
+{
+    /// <summary>
+    /// 获取数据库结构信息
+    /// </summary>
+    public class MsSqlDatabase : Database
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public MsSqlDatabase(string connectionString) : base(connectionString, SqlClientFactory.Instance)
+        {
+
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="connection">数据库链接</param>
+        /// <param name="isOwnsConnection">释放资源时是否释放链接</param>
+        public MsSqlDatabase(SqlConnection connection, bool isOwnsConnection = true)
+            : base(connection, isOwnsConnection)
+        {
+        }
+
+        public override string EncodeColumn(string column)
+        {
+            if (string.IsNullOrEmpty(column)) throw new ArgumentNullException("column");
+            return string.Format("[{0}]", column);
+        }
+
+        public override string EncodeParameterName(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("name");
+            return string.Format("@{0}", name);
+        }
+    }
+}
