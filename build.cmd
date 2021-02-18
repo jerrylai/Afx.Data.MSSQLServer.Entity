c59e74f25f09dc8718049cedb0c957558d829569
jerrylai <jerrylai@aliyun.com>
2021-02-18 12:25:42 +0800

    13.2.1

--- build.cmd
+++ build.cmd
@@ -3,7 +3,8 @@ set Build="%SYSTEMDRIVE%\Program Files (x86)\Microsoft Visual Studio\2019\Enterp
 if exist publish rd /s /q publish
 %Build% "NET40/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
 %Build% "NET45/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
-dotnet build "NETStandard2.0/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release 
+dotnet build "NETStandard2.0/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release
+dotnet build "NETStandard2.1/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release
 cd publish
 del /q/s *.pdb
 del /q/s EntityFramework*
--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("13.1.11")]
-[assembly: AssemblyFileVersion("13.1.11")]
+[assembly: AssemblyVersion("13.2.1")]
+[assembly: AssemblyFileVersion("13.2.1")]
