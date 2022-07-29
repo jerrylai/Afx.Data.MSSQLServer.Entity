6dc10476cfb3a10b8bdbe234c998acdaceb5cc6f
jerrylai <jerrylai@aliyun.com>
2022-07-29 10:50:55 +0800

    13.4.0

--- build.cmd
+++ build.cmd
@@ -1,9 +1,8 @@
 @echo off
-set Build="%SYSTEMDRIVE%\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
+set Build="%SYSTEMDRIVE%\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
 if exist publish rd /s /q publish
-%Build% "NET40/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET45/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" /t:Rebuild /p:Configuration=Release
 dotnet build "NETStandard2.0/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release
+dotnet build "NET6.0/Afx.Data.MSSQLServer.Entity/Afx.Data.MSSQLServer.Entity.csproj" -c Release
 cd publish
 del /q/s *.pdb
 del /q/s EntityFramework*
